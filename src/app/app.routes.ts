638c72c9611afc2628fec057b715de0954df0961
Pap Gergo Rajmund <papgergo16@gmail.com>
2025-12-05 23:37:18 +0100

    Performant component routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,39 +1,49 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { MessagesComponent } from './pages/messages/messages.component';
-import { ProfileComponent } from './pages/profile/profile.component';
-import { SettingsComponent } from './pages/settings/settings.component';
-import { SearchComponent } from './pages/search/search.component';
 import { authGuard, pubicGuard } from './shared/guard/auth.guard';
-import { AuthFormComponent } from './pages/auth-form/auth-form.component';
 
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () =>
-      import('./pages/home/home.component').then((c) => c.HomeComponent),
+    loadComponent: () => import('./pages/home/home.component').then((c) => c.HomeComponent),
   },
   {
     path: 'search',
-    component: SearchComponent,
+    loadComponent: () => import('./pages/search/search.component').then((c) => c.SearchComponent),
+  },
+  {
+    path: 'post/:postId',
+    loadComponent: () =>
+      import('./pages/home/post-page/post-page.component').then((c) => c.PostPageComponent),
+    canActivate: [authGuard],
+  },
+  {
+    path: 'messages/:convId',
+    loadComponent: () =>
+      import('./pages/conversations/conversation/conversation.component').then(
+        (c) => c.ConversationComponent
+      ),
+    canActivate: [authGuard],
   },
   {
     path: 'messages',
-    component: MessagesComponent,
+    loadComponent: () =>
+      import('./pages/conversations/conversations.component').then((c) => c.ConversationsComponent),
     canActivate: [authGuard],
   },
   {
-    path: 'profile',
-    component: ProfileComponent,
-    canActivate: [authGuard],
+    path: 'profile/:userId',
+    loadComponent: () =>
+      import('./pages/profile/profile.component').then((c) => c.ProfileComponent),
   },
   {
     path: 'settings',
-    component: SettingsComponent,
+    loadComponent: () =>
+      import('./pages/settings/settings.component').then((c) => c.SettingsComponent),
   },
   {
     path: 'auth',
-    component: AuthFormComponent,
+    loadComponent: () =>
+      import('./pages/auth-form/auth-form.component').then((c) => c.AuthFormComponent),
     canActivate: [pubicGuard],
   },
   {
